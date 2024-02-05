2c6cd82a7c1d1265dab6a99907bc7837a941d68e
nijgururajofficial <nijgururaja12@gmail.com>
2024-02-05 22:36:48 +0530

    add-register-controller-and-bug-fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,70 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { app } from "../app.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "ok" });
+  // get all the payload from the req.body
+  // validate the fields if empty
+  // check if user exists
+  // check for images
+  // upload to cloudinary, avatar
+  // create user object -create entry in db
+  // remove password and refresh token from response
+  // check for user creation
+  // return res
+
+  const { fullName, username, email, password } = req.body;
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    ApiError(400, "Please provide complete information");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!existedUser) {
+    throw new ApiError(409, "Username or Email already exist.");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLower,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-passowrd -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new user(500, "Something went while registering user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User Registered Successfully"));
 });
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -22,3 +22,5 @@ const uploadOnCloudinary = async (loaclFilePath) => {
     return null;
   }
 };
+
+export { uploadOnCloudinary };
