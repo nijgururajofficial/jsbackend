e3a5a1400d8cf35db8d9b20ab3c44b21f73d2483
nijgururajofficial <nijgururaja12@gmail.com>
2024-02-05 16:44:14 +0530

    add-user-route

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express.app();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
