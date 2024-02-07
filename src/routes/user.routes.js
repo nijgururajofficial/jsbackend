98746a2ed2c4137b615d12ac570b52d0bc14291c
nijgururajofficial <nijgururaja12@gmail.com>
2024-02-07 12:11:09 +0530

    add refreshAccessToken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  registerUser,
+  logOutUser,
+  refreshAccessToken,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -22,5 +27,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 // secured routes
-router.route("logout").post(verifyJWT, logOutUser);
+router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
 export default router;
